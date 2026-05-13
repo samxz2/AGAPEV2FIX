f9c7e3c3affd3f91558cde434e5de5a37259f0a7
samxz2 <sam14tv14@gmail.com>
2026-05-13 13:06:38 -0400

    Precio USDT arreglado

--- src/store/moneda.js
+++ src/store/moneda.js
@@ -1,6 +1,6 @@
 import { reactive } from 'vue';
 
-export const TASA_BCV = 630.00;  
+export const TASA_BCV = 650.00;  
 
 const cargarMonedaGuardada = () => {
   if (typeof window !== 'undefined') {
